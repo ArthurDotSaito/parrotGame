b45ec6170ee86ea513b431f2f18fb9eddf30b2d0
Arthur Saito <arthur_saito1998@hotmail.com>
2022-10-23 01:11:56 -0300

    Added function to render,sort and calculate cards

--- script/game.js
+++ script/game.js
@@ -1,12 +1,20 @@
-let cards = [];
+let cards = [
+    'bobrossparrot',
+    'explodyparrot',
+    'fiestaparrot',
+    'metalparrot',
+    'revertitparrot',
+    'tripletsparrot',
+    'unicornparrot',
+];
+
+let selectedCards = [];
 const cardField = document.querySelector('.cardField');
 let cardQuantity = Number(prompt("Digite o número de cartas do jogo:"));
 
-
-
 while(cardQuantity%2 !== 0 || (cardQuantity < 4)||(cardQuantity > 14)){
     cardQuantity = Number(prompt("Digite o número de cartas do jogo:"));  
- }
+}
 
 function createCardElement(tag, className){
     const element = document.createElement(tag);
@@ -14,14 +22,32 @@ function createCardElement(tag, className){
     return element;
 }
 
+function shuffle(){
+    return Math.random() - 0.5;
+}
 
- function createCard(){
+const shuffledCards = cards.sort(shuffle);
+
+function shuffleParrotDeck(cardQuantity){
+    for(let i = 0; i < cardQuantity/2; i++){
+        selectedCards.push(shuffledCards[i]);
+        selectedCards.push(shuffledCards[i]);
+    }
+    return selectedCards;
+}
+
+shuffleParrotDeck(cardQuantity);
+
+function createCard(parrotImage){
+
     const card = createCardElement('figure', 'classFigure');
     const faceFront = createCardElement('ul', 'face front');
     const faceFrontFigure = createCardElement('div', 'frontBoxFigure');
     const faceBack = createCardElement('ul', 'face back')
     const faceBackFigure = createCardElement('div', 'backBoxFigure');
     
+    faceFrontFigure.style.backgroundImage = `url('./bib/${parrotImage}.gif')`
+
     card.appendChild(faceFront);
     faceFront.appendChild(faceFrontFigure);
     card.appendChild(faceBack);
@@ -29,7 +55,14 @@ function createCardElement(tag, className){
     cardField.appendChild(card);
 
     return card;
- }
+}
 
-//const teste = document.querySelector('.cardField');
-//teste.innerHTML = `<ul>{}</ul>`;
+function loadGame(){
+    selectedCards.sort(shuffle);
+    selectedCards.forEach((parrotImage) => {
+        const card = createCard(parrotImage);
+        cardField.appendChild(card);
+    });
+}
+
+loadGame();
